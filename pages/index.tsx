d96e645de3744a57b0079398c1eadea0260fde63
Karim Saad <karimsaad47@gmail.com>
2021-04-29 23:10:47 +1000

    tailwind + fontawesome

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
-import Head from "next/head";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import styles from "../styles/Home.module.css";
+import "tailwindcss/tailwind.css";
 
 const Home: React.FC = () => {
   return (
-    <div className="pt-5">
-      <h1>Hello!</h1>
-      <p>
+    <div>
+      <div className="text-xl flex justify-center my-2">Hello!</div>
+      <div className="flex justify-center my-1">
         Thanks for visiting our website, we are so appreciative to have you
         here!
-      </p>
-      <p>
+      </div>
+      <div className="flex justify-center my-1">
         Unfortunately, we are still under construction, so please check back
-        soon!
-      </p>
-      <p>
-        In the meantime, check our{" "}
-        <a href="https://www.instagram.com/ichor.jewellery/">our Instagram</a>
-      </p>
+        soon.
+      </div>
+      <div className="flex justify-center text-8xl my-1">
+        <a href="https://www.instagram.com/ichor.jewellery/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+      </div>
     </div>
   );
 };
