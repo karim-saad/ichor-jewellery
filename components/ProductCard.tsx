3e2460a59fb044fcf1ac1875a63120317f276da2
Karim Saad <karim.saad@okendo.io>
2021-07-14 21:52:49 +1000

    responsive images

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,15 +5,20 @@ import React from 'react';
 const ProductCard: React.FC<{ product: Product }> = ({ product }) => {
     const { description, displayName, internalName } = product;
 
-    return <Flex flexDir='column' maxW={['36', null, '56']}>
+    return <Flex flexDir='column' maxW={['40', '52']}>
         <AspectRatio ratio={1 / 1}>
-            <Image src={`/${internalName}/thumbnail.jpg`} alt={displayName} rounded='lg' />
+            <Image
+                srcSet={`${internalName}/0-small.jpg 160w, ${internalName}/0-medium.jpg 210w`}
+                sizes='(max-width: 30em) 160px, 210px'
+                src={`/${internalName}/0-medium.jpg`}
+                alt={displayName} rounded='lg'
+            />
         </AspectRatio>
-        <Text fontSize={['md', null, 'lg']} fontWeight='semibold'>
+        <Text fontSize={['md', 'lg']} fontWeight='semibold'>
             {displayName}
         </Text>
         <Divider />
-        <Text fontSize={['sm', null, 'inherit']}>
+        <Text fontSize={['sm', 'inherit']}>
             {description}
         </Text>
     </Flex>;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { dynamoClient } from '../lib';
 
 export default function LandingPage({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
     return <Layout layoutProps={{ title: 'Ichor Jewellery' }}>
-        <Wrap spacing={['4', null, '8']} mx={['4', null, '8']} my={['2', null, '4']}>
+        <Wrap spacing={['4', null, '8']} mx={['4', null, '8']} my={['2', null, '4']} justify='center'>
             {products.map(product =>
                 <WrapItem key={product.id}>
                     <ProductCard product={product} />
