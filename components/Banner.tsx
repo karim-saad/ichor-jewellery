977c0d70251fd7a19997eef45e44f3fbe5db32c8
Karim Saad <karim.saad@okendo.io>
2021-10-19 21:54:12 +1100

    make redirection easier

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,18 +1,11 @@
 import { Box, Center, HStack, Text } from '@chakra-ui/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Banner: React.FC = () => {
     return <Center as='header' py={1} bgColor='gray.300'>
         <HStack spacing={2}>
-            <Box>
-                <Image
-                    src='/static/technics.svg'
-                    alt='Construction'
-                    height={16}
-                    width={16}
-                />
-            </Box>
             <Text>
                 Under Construction!
             </Text>
@@ -24,6 +17,9 @@ const Banner: React.FC = () => {
                     width={16}
                 />
             </Box>
+            <Link href='https://www.etsy.com/au/shop/IchorJewellery' passHref>
+                Please check out our Etsy for now!
+            </Link>
         </HStack>
     </Center>;
 };
